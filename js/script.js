d7c9f513afb97c990c4be24e7e01e74a7f4d2350
Rashed Hossain <145587722+rashedhossain25@users.noreply.github.com>
2024-01-02 23:51:57 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//js code
+//wow js
+wow = new WOW(
+    {
+    boxClass:     'wow',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+})
+wow.init();
+
+//jq code
+
+// $(document).ready(function(){
+
+// });
